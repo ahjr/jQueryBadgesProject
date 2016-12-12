4089e4fca96aa4a3b13774f3bd41330e3fade15c
alissa <as@iole.co.uk>
2016-12-12 10:54:32 -0800

    update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
   // your code will go here
+  jQuery.ajax({
+    url: 'https://www.codeschool.com/users/iole.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      $.each(response.courses.completed, function(index, course) {
+        var courseDiv = $('<div></div>').addClass("course");
+      	$('<h3>'+course.title+'</h3>').appendTo(courseDiv);
+      	$('<img src="'+course.badge+'""></img>').appendTo(courseDiv);
+      	$('<a href="'+course.url+'" target="_blank" class="btn btn-primary">See Course</a>').appendTo(courseDiv);
+        courseDiv.appendTo($("#badges"));
+      });
+    }
+  });
 
 });
